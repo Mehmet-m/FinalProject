dc8841ecef49e03a9c1248075ac6c39d501aa0eb
Mehmet Tiksöz <mehmettz456@gmail.com>
2021-01-30 23:49:17 +0200

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //IEntity iplement eden bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
